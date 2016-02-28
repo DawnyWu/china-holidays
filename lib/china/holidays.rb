2b40ad1f901e82ae33aa9e23ada5fc6a3effd551
Wu <wuzhaoyang@meizu.com>
2016-02-28 19:53:34 +0800

    add holiday? method to Rails Date and Time class

--- lib/china/holidays.rb
+++ lib/china/holidays.rb
@@ -1,7 +1,35 @@
 require "china/holidays/version"
+require 'json'
 
 module China
   module Holidays
-    # Your code goes here...
+    file = File.read(File.join(__dir__, '..', '..', 'holidays.json'))
+    hash = JSON.parse(file.force_encoding('utf-8'))
+
+    DATES = hash['holidays'].map(&:to_date)
+
+    def self.dates
+      DATES
+    end
+
+    module Date
+      def holiday?
+        DATES.include?(self)
+      end
+    end
+
+    module Time
+      def holiday?
+        DATES.include?(self.to_date)
+      end
+    end
   end
+end
+
+class Date
+  include China::Holidays::Date
+end
+
+class Time
+  include China::Holidays::Time
 end
